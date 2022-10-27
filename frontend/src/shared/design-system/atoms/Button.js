4f6ea79f341ed064379044fef3d51f0ec35ac522
Martin Patočka <patm03@vse.cz>
2022-10-27 13:04:47 +0200

    Fixed button forward ref

--- frontend/src/shared/design-system/atoms/Button.js
+++ frontend/src/shared/design-system/atoms/Button.js
@@ -1,12 +1,14 @@
 import { Button as ChakraButton } from '@chakra-ui/react';
+import { forwardRef } from 'react';
 
-export const Button = (props) => (
+export const Button = forwardRef((props, ref) => (
   <ChakraButton
     minW="150px"
     colorScheme="blue"
     fontWeight="500"
     textTransform="uppercase"
     boxShadow="lg"
+    ref={ref}
     {...props}
   />
-);
+));
