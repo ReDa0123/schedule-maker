1bade00409195eedd269919d28bebc395eb8a35c
Ondrej Altman <ondraalt@gmail.com>
2022-11-01 21:18:49 +0100

    Added sport selection using checkboxes

--- frontend/src/modules/tournament-creator/pages/TournamentCreatorPage.js
+++ frontend/src/modules/tournament-creator/pages/TournamentCreatorPage.js
@@ -3,7 +3,19 @@ import { Form } from 'src/shared/react-hook-form/organisms';
 import {
   FormInput,
   FormSubmitButton,
+  FormCheckbox,
 } from 'src/shared/react-hook-form/molecules';
+
+import { SimpleGrid } from '@chakra-ui/react';
+
+const mockSports = [
+  'Tennis',
+  'Long jump',
+  'Swimming',
+  'High jump',
+  'Chess',
+  'Archery',
+];
 import SubHeader from '../../../shared/design-system/molecules/SubHeader';
 
 const TournamentCreatorPage = () => {
@@ -11,10 +23,15 @@ const TournamentCreatorPage = () => {
     <>
       <SubHeader title={'Create Tournament'} />
     <ContentBox>
-      kontent pro tvorbu tournamentů
+      Tournament settings
       <Form onSubmit={(data) => alert(JSON.stringify(data))}>
-        <FormInput name={'Tournament name'} label={'Tournament name'} />
+        <FormInput name={'Tournament na()me'} label={'Tournament name'} />
         <FormInput name={'Location'} label={'Location'} />
+        <SimpleGrid columns={4}>
+          {mockSports.map((sport) => (
+            <FormCheckbox name={sport} label={sport} key={sport} />
+          ))}
+        </SimpleGrid>
         <FormSubmitButton title={'Save'} showAlert />
       </Form>
     </ContentBox></>
