e5b9357357223b268790aeca99f34e05174b97c7
Ondrej Altman <ondraalt@gmail.com>
2022-11-01 17:55:05 +0100

    Basic form and submit

--- frontend/src/modules/tournament-creator/pages/TournamentCreatorPage.js
+++ frontend/src/modules/tournament-creator/pages/TournamentCreatorPage.js
@@ -1,12 +1,23 @@
 import { ContentBox } from 'src/shared/design-system';
+import { Form } from 'src/shared/react-hook-form/organisms';
+import {
+  FormInput,
+  FormSubmitButton,
+} from 'src/shared/react-hook-form/molecules';
 import SubHeader from '../../../shared/design-system/molecules/SubHeader';
 
 const TournamentCreatorPage = () => {
   return (
     <>
       <SubHeader title={'Create Tournament'} />
-      <ContentBox>kontent pro tvorbu tournamentů</ContentBox>
-    </>
+    <ContentBox>
+      kontent pro tvorbu tournamentů
+      <Form onSubmit={(data) => alert(JSON.stringify(data))}>
+        <FormInput name={'Tournament name'} label={'Tournament name'} />
+        <FormInput name={'Location'} label={'Location'} />
+        <FormSubmitButton title={'Save'} showAlert />
+      </Form>
+    </ContentBox></>
   );
 };
 
