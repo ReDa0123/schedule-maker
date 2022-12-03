74556e8482843ccb6e33ae72e05096814b04388b
Martin Patočka <patm03@vse.cz>
2022-12-03 14:13:33 +0100

    Refactor sport and area mutations.

--- backend/src/modules/area/mutations.js
+++ backend/src/modules/area/mutations.js
@@ -1,6 +1,5 @@
 import { validateArea } from './validations';
-import { map, o, prop, toLower, trim } from 'ramda';
-import { resetBlocks } from '../block/helper';
+import { getGroups, removeGroups, saveGroups } from '../commons/sportsAndAreas';
 
 export const saveAreas = async (
   _,
@@ -13,72 +12,25 @@ export const saveAreas = async (
     tournamentId,
     areas,
   });
-  const areasInLowerCase = map(o(trim, toLower), areas);
-  const existingAreas = await dbConnection.query(`SELECT * FROM area;`);
-  const existingAreasOfTournament = await dbConnection.query(
-    `SELECT * FROM area JOIN tournament_area USING (areaId) WHERE tournamentId = ?`,
-    [tournamentId]
-  );
-  const existingAreasNames = map(o(toLower, prop('name')))(existingAreas);
-  const { newAreas, alreadyCreatedAreas } = areasInLowerCase.reduce(
-    (acc, area) => {
-      if (existingAreasNames.includes(area)) {
-        acc.alreadyCreatedAreas.push(area);
-      } else {
-        acc.newAreas.push(area);
-      }
-      return acc;
-    },
-    { newAreas: [], alreadyCreatedAreas: [] }
-  );
-  const removedAreas = existingAreasOfTournament.filter(
-    (area) => !areasInLowerCase.includes(area.name.toLowerCase())
-  );
 
-  try {
-    await Promise.all(
-      removedAreas.map(async ({ areaId }) => {
-        await resetBlocks('areaId', areaId, dbConnection);
-        return await dbConnection.query(
-          `DELETE FROM tournament_area WHERE tournamentId = ? AND areaId = ?`,
-          [tournamentId, areaId]
-        );
-      })
-    );
-  } catch (error) {
-    throw new Error(error);
-  }
+  const [
+    removedAreas,
+    newAreas,
+    alreadyCreatedAreas,
+    existingAreasOfTournament,
+    existingAreas,
+  ] = await getGroups(areas, 'area', tournamentId, dbConnection);
 
-  await Promise.all(
-    newAreas
-      .map(async (area) => {
-        const dbResponse = await dbConnection.query(
-          `INSERT INTO area (name) VALUES (?)`,
-          [area]
-        );
-        return await dbConnection.query(
-          `INSERT INTO tournament_area (tournamentId, areaId) VALUES (?, ?)`,
-          [tournamentId, dbResponse.insertId]
-        );
-      })
-      .concat(
-        alreadyCreatedAreas.map(async (area) => {
-          if (
-            existingAreasOfTournament.some(
-              ({ name }) => name.toLowerCase() === area
-            )
-          ) {
-            return;
-          }
-          const areaId = existingAreas.find(
-            ({ name }) => name.toLowerCase() === area
-          ).areaId;
-          return await dbConnection.query(
-            `INSERT INTO tournament_area (tournamentId, areaId) VALUES (?, ?)`,
-            [tournamentId, areaId]
-          );
-        })
-      )
+  await removeGroups(removedAreas, 'area', tournamentId, dbConnection);
+
+  await saveGroups(
+    newAreas,
+    'area',
+    alreadyCreatedAreas,
+    existingAreasOfTournament,
+    existingAreas,
+    tournamentId,
+    dbConnection
   );
 
   return 'Areas saved successfully';
---
+++ backend/src/modules/commons/sportsAndAreas.js
@@ -0,0 +1,130 @@
+import {
+  difference,
+  map,
+  o,
+  prop,
+  replace,
+  toLower,
+  toUpper,
+  trim,
+  without,
+} from 'ramda';
+import { resetBlocks } from '../block/helper';
+
+const trimAndLowerCase = map(o(trim, toLower));
+const getNames = map(o(toLower, prop('name')));
+const capitalizeFirstLetter = replace(/^./, toUpper);
+
+export const getGroups = async (
+  inputArray,
+  tableName,
+  tournamentId,
+  dbConnection
+) => {
+  const lowerCaseInputs = trimAndLowerCase(inputArray);
+  const allExistingGroups = await dbConnection.query(
+    `SELECT * FROM ${tableName};`
+  );
+  const allGroupsOfTournament = await dbConnection.query(
+    `SELECT * FROM ${tableName} JOIN tournament_${tableName} USING (${tableName}Id) WHERE tournamentId = ?`,
+    [tournamentId]
+  );
+  const existingGroupNames = getNames(allExistingGroups);
+
+  const newGroups = difference(lowerCaseInputs, existingGroupNames);
+  const alreadyCreatedGroups = without(newGroups, lowerCaseInputs);
+
+  const removedGroups = allGroupsOfTournament.filter(
+    (group) => !lowerCaseInputs.includes(group.name.toLowerCase())
+  );
+
+  return [
+    removedGroups,
+    newGroups,
+    alreadyCreatedGroups,
+    allGroupsOfTournament,
+    allExistingGroups,
+  ];
+};
+
+export const removeGroups = async (
+  removedGroups,
+  tableName,
+  tournamentId,
+  dbConnection,
+  checkBlocks
+) =>
+  Promise.all(
+    removedGroups.map(async (group) => {
+      const { name } = group;
+      const idName = `${tableName}Id`;
+      const id = group[idName];
+
+      if (checkBlocks) {
+        const assignedGroups = await dbConnection.query(
+          `SELECT blockId FROM block WHERE tournamentId = ? AND ${idName} = ?`,
+          [tournamentId, id]
+        );
+
+        if (assignedGroups.length > 0) {
+          throw new Error(
+            `${capitalizeFirstLetter(
+              tableName
+            )} ${name} is assigned to a block. Please remove the ${tableName} from the block before removing it from the tournament.`
+          );
+        }
+      } else {
+        await resetBlocks(idName, id, dbConnection);
+      }
+      return await dbConnection.query(
+        `DELETE FROM tournament_${tableName} WHERE tournamentId = ? AND ${idName} = ?`,
+        [tournamentId, id]
+      );
+    })
+  );
+
+export const saveGroups = async (
+  newGroups,
+  tableName,
+  alreadyCreatedGroups,
+  existingGroupsOfTournament,
+  existingGroups,
+  tournamentId,
+  dbConnection
+) => {
+  const idName = `${tableName}Id`;
+  return Promise.all(
+    //insert new groups
+    newGroups
+      .map(async (group) => {
+        const dbResponse = await dbConnection.query(
+          `INSERT INTO ${tableName} (name) VALUES (?)`,
+          [group]
+        );
+        return await dbConnection.query(
+          `INSERT INTO tournament_${tableName} (tournamentId, ${idName}) VALUES (?, ?)`,
+          [tournamentId, dbResponse.insertId]
+        );
+      })
+      //insert already created groups
+      .concat(
+        alreadyCreatedGroups.map(async (group) => {
+          // Check if the group is already assigned to the tournament
+          if (
+            existingGroupsOfTournament.some(
+              ({ name }) => name.toLowerCase() === group
+            )
+          ) {
+            return;
+          }
+          const groupId = existingGroups.find(
+            ({ name }) => name.toLowerCase() === group
+          )[idName];
+          return await dbConnection.query(
+            `INSERT INTO tournament_${tableName} (tournamentId, ${idName}) VALUES (?, ?)`,
+            [tournamentId, groupId]
+          );
+        })
+      )
+  );
+};
