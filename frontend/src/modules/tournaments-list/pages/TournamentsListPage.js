1a1ddf68ed45e0245f1ac161c59af2c426da4227
MaPatocka <martin.patocka@generaliceska.cz>
2022-12-06 16:52:26 +0100

    Added sorting of tournaments

--- frontend/src/modules/tournaments-list/pages/TournamentsListPage.js
+++ frontend/src/modules/tournaments-list/pages/TournamentsListPage.js
@@ -1,5 +1,8 @@
 import { TournamentListTemplate } from '../templates';
 import { gql, useQuery } from '@apollo/client';
+import { useMemo } from 'react';
+import { useAuth } from '../../auth';
+import { sort } from 'ramda';
 
 const GET_TOURNAMENTS_QUERY = gql`
   query Tournaments {
@@ -16,10 +19,27 @@ const GET_TOURNAMENTS_QUERY = gql`
 
 const TournamentsListPage = () => {
   const tournamentsFetcher = useQuery(GET_TOURNAMENTS_QUERY);
+  const auth = useAuth();
+  const userId = Number(auth?.user?.userId);
+  const tournaments = useMemo(() => {
+    const sortFn = !isNaN(userId)
+      ? (
+          { userId: userId1, tournamentId: tournamentId1 },
+          { userId: userId2, tournamentId: tournamentId2 }
+        ) =>
+          (userId1 === userId && userId2 === userId) ||
+          (userId1 !== userId && userId2 !== userId)
+            ? tournamentId2 - tournamentId1
+            : userId1 === userId
+            ? -1
+            : 1
+      : (a, b) => b.tournamentId - a.tournamentId;
+    return sort(sortFn, tournamentsFetcher.data?.tournaments ?? []);
+  }, [userId, tournamentsFetcher]);
 
   return (
     <TournamentListTemplate
-      tournaments={tournamentsFetcher.data?.tournaments}
+      tournaments={tournaments}
       isLoading={tournamentsFetcher.loading}
       error={tournamentsFetcher.error}
     />
